4bdebe24cf0377f79e924f28a50d5438fb57e992
Fabio Hiroki <fabiothiroki@gmail.com>
2019-07-05 07:05:53 -0300

    New site using hugo

---
+++ .cache/async-requires.js
@@ -0,0 +1,14 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+exports.components = {
+  "component---themes-identity-pages-index-js": require("gatsby-module-loader?name=component---themes-identity-pages-index-js!/Users/fabiothiroki/Repositories/fabiothiroki.github.io/themes/identity/pages/index.js")
+}
+
+exports.json = {
+  "index.json": require("gatsby-module-loader?name=path---index!/Users/fabiothiroki/Repositories/fabiothiroki.github.io/.cache/json/index.json")
+}
+
+exports.layouts = {
+
+}
